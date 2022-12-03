730dce3db71d3fdfbed23f997326bd2f4b7c0bfc
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2022-12-03 23:53:33 +0530

    Create JustOnlyForPullRequest.java

---
+++ JustOnlyForPullRequest.java
@@ -0,0 +1,6 @@
+// Hi i created this file just only for creating and accepting a pull request.
+public class Atul{
+  public static void main(String[] args){
+      System.out.println("Hello world");
+  }
+}
